7261afd3f4c643c02e3712a81df7fb6823393422
asafpamzn <asafp@amazon.com>
2025-08-17 10:57:44 +0300

    updates

--- valkey_glide_cluster.stub.php
+++ valkey_glide_cluster.stub.php
@@ -298,7 +298,7 @@ class ValkeyGlideCluster
     /**
      * @see ValkeyGlide::exec()
      */
-    /* TODOpublic function exec(): array|false;*/
+    public function exec(): array|false;
 
     /**
      * @see ValkeyGlide::exists
@@ -602,7 +602,7 @@ class ValkeyGlideCluster
     /* We only support ValkeyGlide::MULTI in ValkeyGlideCluster but take the argument
        so we can test MULTI..EXEC with ValkeyGlideTest.php and in the event
        we add pipeline support in the future. */
-    /* TODO public function multi(int $value = ValkeyGlide::MULTI): ValkeyGlideCluster|bool; */
+    public function multi(int $value = ValkeyGlide::MULTI): ValkeyGlideCluster|bool;
 
     /**
      * @see ValkeyGlide::object
