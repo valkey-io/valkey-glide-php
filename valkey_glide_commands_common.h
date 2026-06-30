47d82d658f4c83bb95965649415b5ea781628224
Prateek Kumar <prateek.kumar@improving.com>
2026-06-30 15:49:26 -0700

    PHP: Fix lint and update changelog
    Signed-off-by: Prateek Kumar <prateek.kumar@improving.com>

--- valkey_glide_commands_common.h
+++ valkey_glide_commands_common.h
@@ -690,18 +690,18 @@ int execute_unlink_command(zval* object, int argc, zval* return_value, zend_clas
         RETURN_FALSE;                                                               \
     }
 
-#define BGSAVE_METHOD_IMPL(class_name)                                             \
-    PHP_METHOD(class_name, bgSave) {                                               \
-        if (execute_bgsave_command(getThis(),                                      \
-                                    ZEND_NUM_ARGS(),                                \
-                                    return_value,                                   \
-                                    strcmp(#class_name, "ValkeyGlideCluster") == 0  \
-                                        ? get_valkey_glide_cluster_ce()             \
-                                        : get_valkey_glide_ce())) {                 \
-            return;                                                                 \
-        }                                                                           \
-        zval_dtor(return_value);                                                    \
-        RETURN_FALSE;                                                               \
+#define BGSAVE_METHOD_IMPL(class_name)                                            \
+    PHP_METHOD(class_name, bgSave) {                                              \
+        if (execute_bgsave_command(getThis(),                                     \
+                                   ZEND_NUM_ARGS(),                               \
+                                   return_value,                                  \
+                                   strcmp(#class_name, "ValkeyGlideCluster") == 0 \
+                                       ? get_valkey_glide_cluster_ce()            \
+                                       : get_valkey_glide_ce())) {                \
+            return;                                                               \
+        }                                                                         \
+        zval_dtor(return_value);                                                  \
+        RETURN_FALSE;                                                             \
     }
 
 #define TIME_METHOD_IMPL(class_name)                                            \
