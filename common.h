137b5b7811ca95bee7554a7e928dd6d5cdd473df
Prateek Kumar <prateek.kumar@improving.com>
2026-08-20 09:00:06 -0700

    Update version in common.h and package.xml
    Signed-off-by: Prateek Kumar <prateek.kumar@improving.com>

--- common.h
+++ common.h
@@ -11,7 +11,7 @@
 #include "valkey_glide_address_resolver.h"
 
 /* ValkeyGlidePHP version */
-#define VALKEY_GLIDE_PHP_VERSION "1.1.2"
+#define VALKEY_GLIDE_PHP_VERSION "1.2.0-rc1"
 
 #define VALKEY_GLIDE_PHP_GET_OBJECT(class_entry, o) \
     (class_entry*) ((char*) o - XtOffsetOf(class_entry, std))
